41ee56c3bb979f29897a476b90ae63d853900f6d
Ryan Morrison <ryanam26@yahoo.com>
2020-05-09 10:35:30 -0400

    searchbar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -26,7 +26,9 @@ export class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
-  }
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  };
 
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -53,13 +55,17 @@ export class App extends React.Component {
     const trackUris = this.state.playlistTracks.map(track => track.uri);
   }
 
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Spotify<span className="highlight">API</span></h1>
         <div className="App">
 
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
 
           <div className="App-playlist">
 
@@ -69,7 +75,8 @@ export class App extends React.Component {
             <Playlist playListName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks} 
             onRemove={this.removeTrack}
-            onNameChange={this.updatePlaylistName} />
+            onNameChange={this.updatePlaylistName}
+            onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
